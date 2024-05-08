4a72611de22392a6f209cc9bf462fb1e6b965b07
jxshnpreet <2002jashanpreet@gmail.com>
2024-05-08 14:56:15 +0530

    final push

---
+++ src/components/WhyChooseUs.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import { StickyScroll } from "../components/Ui/sticky-scroll-reveal";
+
+import img01 from '../../public/courses/blues.jpg';
+import img02 from '../../public/courses/classical-music.jpg';
+import img03 from '../../public/courses/drumming.jpg';
+import img04 from '../../public/courses/guitar.jpg';
+import img05 from '../../public/courses/piano.jpg';
+import img06 from '../../public/courses/song-writing.jpg';
+
+const musicSchoolContent = [
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+      imageUrl: img01
+    },
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+      imageUrl: img02
+    },
+    {
+      title: 'Discover Your Sound with Us: A Personal Journey in Music Mastery',
+      description:
+        'Embark on a musical journey that’s uniquely yours. Our personalized instruction adapts to your individual needs, setting the stage for unparalleled growth and creativity. At our music school, your aspirations meet our dedicated support, creating a harmonious path to mastery.',
+      imageUrl: img03
+    },
+    {
+      title: 'Live Feedback & Engagement',
+      description:
+        'Immerse yourself in an interactive learning experience where feedback is immediate, just like real-time changes in a collaborative project. This approach enhances your understanding and mastery of music concepts and performance techniques.',
+      imageUrl: img04
+    },
+    {
+      title: 'Cutting-Edge Curriculum',
+      description:
+        'Our curriculum is continuously updated to include the latest music education trends and technologies, ensuring you’re always learning with the most current and effective methods. Say goodbye to outdated materials and welcome an education that evolves with the industry.',
+      imageUrl: img05
+    },
+    {
+      title: 'Limitless Learning Opportunities',
+      description:
+        'With our expansive resource library and dynamic course offerings, you’ll never find yourself without something new to explore. Our platform provides continuous opportunities for growth, ensuring your musical skills are always advancing.',
+      imageUrl: img06
+    },
+];
+
+function WhyChooseUs() {
+  return (
+    <div>
+      <StickyScroll content={musicSchoolContent} />
+    </div>
+  );
+}
+
+export default WhyChooseUs;
